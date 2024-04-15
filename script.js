adbfbe4af7a579caee14dae0e2d484c3d666e1d5
Sumit <sumitsingh80056@gmail.com>
2024-04-15 13:33:13 +0530

    Added Formatting to documents

--- script.js
+++ script.js
@@ -2,10 +2,10 @@
 
 var navlinks = document.getElementById("navlinks");
 
-        function showMenu(){
-             navlinks.style.right = "0";
-        }
+function showMenu() {
+  navlinks.style.right = "0";
+}
 
-        function hideMenu(){
-            navlinks.style.right = "-200px";
-        }
+function hideMenu() {
+  navlinks.style.right = "-200px";
+}
